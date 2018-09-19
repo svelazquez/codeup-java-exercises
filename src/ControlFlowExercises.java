dd76cc7e78598c10ad09537e54f3a5244170b5ac
Stephanie Velazquez <svelazquez@Stephanies-MacBook-Air.local>
2018-09-19 12:24:06 -0500

    exercise 4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,15 +3,18 @@ import java.util.*;
 public class ControlFlowExercises {
     public static void main(String[] args) {
        Scanner s  = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int userInt = s.nextInt();
-        System.out.println("Table Incoming");
-        System.out.println();
-        System.out.println(" number   |   squared  |   cubed   ");
-        System.out.println("--------  | ---------- | --------- ");
-        System.out.printf("%-10s| %-10s | %-10s",
-                            userInt,
-                            userInt * userInt,
-                            userInt * userInt * userInt);
+        System.out.println("Enter a grade");
+        int g = s.nextInt();
+
+
+        char lGrade = ' ';
+        if(g >= 0){
+            if(g < 60) lGrade = 'F';
+            else if(g <= 66) lGrade = 'D';
+            else if(g<= 79) lGrade = 'C';
+            else if (g <= 89) lGrade= 'B';
+            else lGrade = 'A';
+        }
+        System.out.println(lGrade);
     }
 }
