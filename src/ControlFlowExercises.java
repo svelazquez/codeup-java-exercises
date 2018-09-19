a09406f198cee45c884ba6a270a76846c3789b66
Stephanie Velazquez <svelazquez@Stephanies-MacBook-Air.local>
2018-09-19 11:49:05 -0500

    exercise 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,40 +1,17 @@
+import java.util.*;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while(i<=15){
-            System.out.println(i++);
-        }
-
+       Scanner s  = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int userInt = s.nextInt();
+        System.out.println("Table Incoming");
         System.out.println();
-
-        for(int j = 5; j<=15; j++){
-            System.out.println(j);
-        }
-
-
-        long x = 2;
-        do{
-            System.out.println(x);
-            x*=x;
-
-
-        }while(x<1000000);
-
-        for(long y = 2; y<1000000; y*=y){
-            System.out.println(y);
-        }
-
-
-        for(i = 0; i<=100; i++){
-            if(i%3==0){
-                System.out.println("fizz");
-            } else if (i%5==0){
-                System.out.println("buzz");
-            } else if (i%3==0 && i%5 ==0){
-                System.out.println("fizz buzz");
-            } else {
-                System.out.println(i);
-            }
-        }
+        System.out.println(" number   |   squared  |   cubed   ");
+        System.out.println("--------  | ---------- | --------- ");
+        System.out.printf("%-10s| %-10s | %-10s",
+                            userInt,
+                            userInt * userInt,
+                            userInt * userInt * userInt);
     }
 }
